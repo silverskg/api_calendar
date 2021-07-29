a3d13608bc596e6b1daf00649c06cefb7cc9cc60
silverskg <chokichoki_gm@i.softbank.jp>
2021-07-29 15:21:25 +0900

    イベント作成APIを作成

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,26 @@
+class EventsController < ApplicationController
+  def index
+    # イベントデータの一覧を返す
+    render json: Event.all
+  end
+
+  def show
+    # 指定したidのイベントを返す
+    render json: Event.find(params[:id])
+  end
+
+  def create
+    event = Event.new(event_params)
+    if event.save
+      render json: event
+    else
+      render json: event.errors, status: 422
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Event.create!(name: '予定1', start: '2021-07-29 19:00:00', end: '2021-07-29 20:00:00')
+Event.create!(name: '予定2', start: '2021-07-30 19:00:00', end: '2021-07-29 20:00:00')
+Event.create!(name: '予定3', start: '2021-07-31 19:00:00', end: '2021-07-29 20:00:00')
+Event.create!(name: '予定4', start: '2021-08-01 19:00:00', end: '2021-07-29 20:00:00')
